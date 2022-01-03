85431c0951ea3497e3a742c64cadeac1630f9853
SUDIPTA PADHI <63278866+sudiptapadhi@users.noreply.github.com>
2022-01-03 12:14:52 +0530

    SUDIPTA PADHI

---
+++ index.js
@@ -0,0 +1,420 @@
+var pay1 = document.getElementById('payment1');
+var spot_left1 = document.getElementById('spot1');
+var count1 = 50;
+
+function main1(event1) {
+    count1--;
+    console.log(count1);
+    spot_left1.innerText = count1;
+    if (count1 <= -1) {
+        spot_left1.innerText = "0";
+        console.log("full");
+        pay1.innerText = "Full";
+        event1.preventDefault();
+    }
+
+}
+pay1.addEventListener('click', main1);
+
+
+var pay2 = document.getElementById('payment2');
+var spot_left2 = document.getElementById('spot2');
+var count2 = 50;
+
+function main2(event2) {
+    count2--;
+    console.log(count2);
+    spot_left2.innerText = count2;
+    if (count2 <= -1) {
+        spot_left2.innerText = "0";
+        console.log("full");
+        pay2.innerText = "Full";
+        event2.preventDefault();
+    }
+
+}
+pay2.addEventListener('click', main2);
+
+
+var pay3 = document.getElementById('payment3');
+var spot_left3 = document.getElementById('spot3');
+var count3 = 50;
+
+function main3(event3) {
+    count3--;
+    console.log(count3);
+    spot_left3.innerText = count3;
+    if (count3 <= -1) {
+        spot_left3.innerText = "0";
+        console.log("full");
+        pay3.innerText = "Full";
+        event3.preventDefault();
+    }
+
+}
+pay3.addEventListener('click', main3);
+
+
+var pay4 = document.getElementById('payment4');
+var spot_left4 = document.getElementById('spot4');
+var count4 = 50;
+
+function main4(event4) {
+    count4--;
+    console.log(count4);
+    spot_left4.innerText = count4;
+    if (count4 <= -1) {
+        spot_left4.innerText = "0";
+        console.log("full");
+        pay4.innerText = "Full";
+        event4.preventDefault();
+    }
+
+}
+pay4.addEventListener('click', main4);
+
+
+
+var pay5 = document.getElementById('payment5');
+var spot_left5 = document.getElementById('spot5');
+var count5 = 50;
+
+function main5(event5) {
+    count5--;
+    console.log(count5);
+    spot_left5.innerText = count5;
+    if (count5 <= -1) {
+        spot_left5.innerText = "0";
+        console.log("full");
+        pay5.innerText = "Full";
+        event5.preventDefault();
+    }
+
+}
+pay5.addEventListener('click', main5);
+
+
+
+var pay6 = document.getElementById('payment6');
+var spot_left6 = document.getElementById('spot6');
+var count6 = 50;
+
+function main6(event6) {
+    count6--;
+    console.log(count6);
+    spot_left6.innerText = count6;
+    if (count6 <= -1) {
+        spot_left6.innerText = "0";
+        console.log("full");
+        pay6.innerText = "Full";
+        event6.preventDefault();
+    }
+
+}
+pay6.addEventListener('click', main6);
+
+
+
+var pay7 = document.getElementById('payment7');
+var spot_left7 = document.getElementById('spot7');
+var count7 = 50;
+
+function main7(event7) {
+    count7--;
+    console.log(count7);
+    spot_left7.innerText = count7;
+    if (count7 <= -1) {
+        spot_left7.innerText = "0";
+        console.log("full");
+        pay7.innerText = "Full";
+        event7.preventDefault();
+    }
+
+}
+pay7.addEventListener('click', main7);
+
+
+var pay8 = document.getElementById('payment8');
+var spot_left8 = document.getElementById('spot8');
+var count8 = 50;
+
+function main8(event8) {
+    count8--;
+    console.log(count8);
+    spot_left8.innerText = count8;
+    if (count8 <= -1) {
+        spot_left8.innerText = "0";
+        console.log("full");
+        pay8.innerText = "Full";
+        event8.preventDefault();
+    }
+
+}
+pay8.addEventListener('click', main8);
+
+var pay9 = document.getElementById('payment9');
+var spot_left9 = document.getElementById('spot9');
+var count9 = 50;
+
+function main9(event9) {
+    count9--;
+    console.log(count9);
+    spot_left9.innerText = count9;
+    if (count9 <= -1) {
+        spot_left9.innerText = "0";
+        console.log("full");
+        pay9.innerText = "Full";
+        event9.preventDefault();
+    }
+
+}
+pay9.addEventListener('click', main9);
+
+var pay10 = document.getElementById('payment10');
+var spot_left10 = document.getElementById('spot10');
+var count10 = 50;
+
+function main10(event10) {
+    count10--;
+    console.log(count10);
+    spot_left10.innerText = count10;
+    if (count10 <= -1) {
+        spot_left10.innerText = "0";
+        console.log("full");
+        pay10.innerText = "Full";
+        event10.preventDefault();
+    }
+
+}
+pay10.addEventListener('click', main10);
+
+var pay11 = document.getElementById('payment11');
+var spot_left11 = document.getElementById('spot11');
+var count11 = 50;
+
+function main11(event11) {
+    count11--;
+    console.log(count11);
+    spot_left11.innerText = count11;
+    if (count11 <= -1) {
+        spot_left11.innerText = "0";
+        console.log("full");
+        pay11.innerText = "Full";
+        event11.preventDefault();
+    }
+
+}
+pay11.addEventListener('click', main11);
+
+var pay12 = document.getElementById('payment12');
+var spot_left12 = document.getElementById('spot12');
+var count12 = 50;
+
+function main12(event12) {
+    count12--;
+    console.log(count12);
+    spot_left12.innerText = count12;
+    if (count12 <= -1) {
+        spot_left12.innerText = "0";
+        console.log("full");
+        pay12.innerText = "Full";
+        event12.preventDefault();
+    }
+
+}
+pay12.addEventListener('click', main12);
+
+var pay13 = document.getElementById('payment13');
+var spot_left13 = document.getElementById('spot13');
+var count13 = 50;
+
+function main13(event13) {
+    count13--;
+    console.log(count13);
+    spot_left13.innerText = count13;
+    if (count13 <= -1) {
+        spot_left13.innerText = "0";
+        console.log("full");
+        pay13.innerText = "Full";
+        event13.preventDefault();
+    }
+
+}
+pay13.addEventListener('click', main13);
+
+var pay14 = document.getElementById('payment14');
+var spot_left14 = document.getElementById('spot14');
+var count14 = 50;
+
+function main14(event14) {
+    count14--;
+    console.log(count14);
+    spot_left14.innerText = count14;
+    if (count14 <= -1) {
+        spot_left14.innerText = "0";
+        console.log("full");
+        pay14.innerText = "Full";
+        event14.preventDefault();
+    }
+
+}
+pay14.addEventListener('click', main14);
+
+var pay15 = document.getElementById('payment15');
+var spot_left15 = document.getElementById('spot15');
+var count15 = 50;
+
+function main15(event15) {
+    count15--;
+    console.log(count15);
+    spot_left15.innerText = count15;
+    if (count15 <= -1) {
+        spot_left15.innerText = "0";
+        console.log("full");
+        pay15.innerText = "Full";
+        event15.preventDefault();
+    }
+
+}
+pay15.addEventListener('click', main15);
+
+var pay16 = document.getElementById('payment16');
+var spot_left16 = document.getElementById('spot16');
+var count16 = 50;
+
+function main16(event16) {
+    count16--;
+    console.log(count16);
+    spot_left16.innerText = count16;
+    if (count16 <= -1) {
+        spot_left16.innerText = "0";
+        console.log("full");
+        pay16.innerText = "Full";
+        event16.preventDefault();
+    }
+
+}
+pay16.addEventListener('click', main16);
+
+var pay17 = document.getElementById('payment17');
+var spot_left17 = document.getElementById('spot17');
+var count17 = 50;
+
+function main17(event17) {
+    count17--;
+    console.log(count17);
+    spot_left17.innerText = count17;
+    if (count17 <= -1) {
+        spot_left17.innerText = "0";
+        console.log("full");
+        pay17.innerText = "Full";
+        event17.preventDefault();
+    }
+
+}
+pay17.addEventListener('click', main17);
+
+var pay18 = document.getElementById('payment18');
+var spot_left18 = document.getElementById('spot18');
+var count18 = 50;
+
+function main18(event18) {
+    count18--;
+    console.log(count18);
+    spot_left18.innerText = count18;
+    if (count18 <= -1) {
+        spot_left18.innerText = "0";
+        console.log("full");
+        pay18.innerText = "Full";
+        event18.preventDefault();
+    }
+
+}
+pay18.addEventListener('click', main18);
+
+var pay19 = document.getElementById('payment19');
+var spot_left19 = document.getElementById('spot19');
+var count19 = 50;
+
+function main19(event19) {
+    count19--;
+    console.log(count19);
+    spot_left19.innerText = count19;
+    if (count19 <= -1) {
+        spot_left19.innerText = "0";
+        console.log("full");
+        pay19.innerText = "Full";
+        event19.preventDefault();
+    }
+
+}
+pay19.addEventListener('click', main19);
+
+var pay20 = document.getElementById('payment20');
+var spot_left20 = document.getElementById('spot20');
+var count20 = 50;
+
+function main20(event20) {
+    count20--;
+    console.log(count20);
+    spot_left20.innerText = count20;
+    if (count20 <= -1) {
+        spot_left20.innerText = "0";
+        console.log("full");
+        pay20.innerText = "Full";
+        event20.preventDefault();
+    }
+
+}
+pay20.addEventListener('click', main20);
+
+var pay21 = document.getElementById('payment21');
+var spot_left21 = document.getElementById('spot21');
+var count21 = 50;
+
+function main21(event21) {
+    count21--;
+    console.log(count21);
+    spot_left21.innerText = count21;
+    if (count21 <= -1) {
+        spot_left21.innerText = "0";
+        console.log("full");
+        pay21.innerText = "Full";
+        event21.preventDefault();
+    }
+
+}
+pay21.addEventListener('click', main21);
+
+
+
+
+
+var pay22 = document.getElementById('payment22');
+var spot_left22 = document.getElementById('spot22');
+var count22 = 50;
+
+function main22(event22) {
+    count22--;
+    console.log(count22);
+    spot_left22.innerText = count22;
+    if (count22 <= -1) {
+        spot_left22.innerText = "0";
+        console.log("full");
+        pay22.innerText = "Full";
+        event22.preventDefault();
+    }
+
+}
+pay22.addEventListener('click', main22);
+
+
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
